94d53e2e8ab2653d62a2db74e7639fb500603a90
JO Hyeong-Ryeol <ryeol@outlook.com>
2013-07-07 19:36:09 +0900

    Removed outdated sample.

--- Nicenis/NicenisTestGui/MainWindow.xaml.cs
+++ Nicenis/NicenisTestGui/MainWindow.xaml.cs
@@ -6,20 +6,7 @@
  * Copyright (C) 2013 JO Hyeong-Ryeol. All rights reserved.
  */
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace NicenisTestGui
 {
@@ -31,22 +18,6 @@ namespace NicenisTestGui
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void CreateDragNDropTestWindowButton_Click(object sender, RoutedEventArgs e)
-        {
-            DragDropTestWindow window = new DragDropTestWindow();
-            window.Owner = this;
-
-            window.Show();
-        }
-
-        private void CreateHoverTestWindowButton_Click(object sender, RoutedEventArgs e)
-        {
-            HoverTestWindow window = new HoverTestWindow();
-            window.Owner = this;
-
-            window.Show();
         }
 
         private void CreateDragMoverTestWindowButton_Click(object sender, RoutedEventArgs e)
@@ -68,6 +39,14 @@ namespace NicenisTestGui
         private void CreateDragSourceNDropTargetTestWindow_Click(object sender, RoutedEventArgs e)
         {
             DragSourceNDropTargetTestWindow window = new DragSourceNDropTargetTestWindow();
+            window.Owner = this;
+
+            window.Show();
+        }
+
+        private void CreateHoverTestWindowButton_Click(object sender, RoutedEventArgs e)
+        {
+            HoverTestWindow window = new HoverTestWindow();
             window.Owner = this;
 
             window.Show();
