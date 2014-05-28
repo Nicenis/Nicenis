191c3353b0b20ad1152f58ffec69459eec1cbca1
JO Hyeong-Ryeol <ryeol@outlook.com>
2014-05-29 03:46:40 +0900

    Added the IFreezable interface.

---
+++ Nicenis/Nicenis/ComponentModel/IFreezable.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * Author   JO Hyeong-Ryeol
+ * Since    2014.05.29
+ * Version  $Id$
+ * 
+ * This file is a part of the Nicenis project.
+ * https://nicenis.codeplex.com
+ * 
+ * Copyright (C) 2014 JO Hyeong-Ryeol. All rights reserved.
+ */
+
+namespace Nicenis.ComponentModel
+{
+    /// <summary>
+    /// Represents an object that can be frozen. If the object is frozen, it is immutable.
+    /// </summary>
+    public interface IFreezable
+    {
+        /// <summary>
+        /// Gets a value that indicates whether the object can be made unmodifiable. 
+        /// </summary>
+        bool CanFreeze { get; }
+
+        /// <summary>
+        /// Gets a value that indicates whether the object is currently modifiable.
+        /// </summary>
+        bool IsFrozen { get; }
+
+        /// <summary>
+        /// Makes the current object unmodifiable and sets its IsFrozen property to true.
+        /// </summary>
+        void Freeze();
+    }
+}
