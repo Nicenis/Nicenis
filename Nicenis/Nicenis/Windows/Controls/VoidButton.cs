1981045c94b150abdab334c2aa5bdb66e0fbc01e
JO Hyeong-Ryeol <ryeol@outlook.com>
2014-06-21 17:15:19 +0900

    Added the VoidButton control.

---
+++ Nicenis/Nicenis/Windows/Controls/VoidButton.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Author   JO Hyeong-Ryeol
+ * Since    2014.06.21
+ * Version  $Id$
+ * 
+ * This file is a part of the Nicenis project.
+ * https://nicenis.codeplex.com
+ * 
+ * Copyright (C) 2014 JO Hyeong-Ryeol. All rights reserved.
+ */
+
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Nicenis.Windows.Controls
+{
+    /// <summary>
+    /// Represents a button without any visual element.
+    /// </summary>
+    public class VoidButton : Button
+    {
+        #region Constructors
+
+        /// <summary>
+        /// The static constructor.
+        /// </summary>
+        static VoidButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(VoidButton), new FrameworkPropertyMetadata(typeof(VoidButton)));
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the VoidButton class.
+        /// </summary>
+        public VoidButton() { }
+
+        #endregion
+    }
+}
