3cd961c30fc89770e5e02833d4f69508aac0ac8d
JO Hyeong-Ryeol <hyeongryeol@hotmail.com>
2014-09-18 04:45:28 +0900

    Added WeakEvent.

---
+++ Nicenis/Nicenis/WeakEvent.cs
@@ -0,0 +1,284 @@
+﻿/*
+ * Author   JO Hyeong-Ryeol
+ * Since    2014.09.18
+ * Version  $Id$
+ * 
+ * This file is a part of the Nicenis project.
+ * https://nicenis.codeplex.com
+ * 
+ * Copyright (C) 2014 JO Hyeong-Ryeol. All rights reserved.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Nicenis
+{
+    #region WeakEventImplementation
+
+    /// <summary>
+    /// Provides most implementation for WeakEvent.
+    /// </summary>
+    /// <typeparam name="TEventHandler">The event handler type.</typeparam>
+    internal class WeakEventImplementation<TEventHandler> where TEventHandler : class
+    {
+        IEnumerable<WeakReference> _weakHandlers = Enumerable.Empty<WeakReference>();
+
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        public WeakEventImplementation() { }
+
+        #endregion
+
+
+        #region Public Methods
+
+        /// <summary>
+        /// Adds an event handler.
+        /// </summary>
+        /// <param name="value">The event handler to add.</param>
+        public void Add(TEventHandler value)
+        {
+            if (value == null)
+                throw new ArgumentNullException("value");
+
+            var weakHandlers = _weakHandlers;
+            var newWeakHandlers = new WeakReference[weakHandlers.Count() + 1];
+
+            // Copies existing event handlers.
+            int index = 0;
+            foreach (var weakHandler in weakHandlers)
+                newWeakHandlers[index++] = weakHandler;
+
+            // Copies the new event handler.
+            newWeakHandlers[index] = new WeakReference(value);
+
+            // Updatea the weak event handler collection.
+            _weakHandlers = newWeakHandlers;
+        }
+
+        /// <summary>
+        /// Removes the event handler.
+        /// </summary>
+        /// <param name="value">The event handler to remove.</param>
+        public void Remove(TEventHandler value)
+        {
+            if (value == null)
+                throw new ArgumentNullException("value");
+
+            // Finds the weak event handler to delete.
+            var weakHandlers = _weakHandlers;
+            var weakHandlerToDelete = weakHandlers.FirstOrDefault
+            (
+                weakHandler =>
+                {
+                    TEventHandler handler = weakHandler.Target as TEventHandler;
+                    if (handler == null)
+                        return false;
+
+                    return handler.Equals(value);
+                }
+            );
+
+            // If there is no weak event handler to delete...
+            if (weakHandlerToDelete == null)
+                return;
+
+            var newWeakHandlers = new WeakReference[weakHandlers.Count() - 1];
+            int index = 0;
+            foreach (var weakHandler in weakHandlers)
+            {
+                // Skips the weak event handler to delete.
+                if (weakHandler == weakHandlerToDelete)
+                    continue;
+
+                newWeakHandlers[index++] = weakHandler;
+            }
+
+            // Updatea the weak event handler collection.
+            _weakHandlers = newWeakHandlers;
+        }
+
+        /// <summary>
+        /// Raises the event.
+        /// </summary>
+        /// <param name="invoker">The action that calls the event handler.</param>
+        public void Raise(Action<TEventHandler> invoker)
+        {
+            if (invoker == null)
+                throw new ArgumentNullException("invoker");
+
+            var weakHandlers = _weakHandlers;
+            List<WeakReference> weakHandlersToDelete = null; ;
+
+            // For each weak handler...
+            foreach (var weakHandler in weakHandlers)
+            {
+                TEventHandler handler = weakHandler.Target as TEventHandler;
+                if (handler == null)
+                {
+                    if (weakHandlersToDelete == null)
+                        weakHandlersToDelete = new List<WeakReference>();
+
+                    // Collects the garbage collected event handler.
+                    weakHandlersToDelete.Add(weakHandler);
+                    continue;
+                }
+
+                // Invokes the event handler.
+                invoker(handler);
+            }
+
+            // If there is no event handler garbage collected
+            if (weakHandlersToDelete == null)
+                return;
+
+            // If all event handlers are garbage collected
+            int weakHandlerCount = weakHandlers.Count();
+            if (weakHandlersToDelete.Count == weakHandlerCount)
+            {
+                _weakHandlers = Enumerable.Empty<WeakReference>();
+                return;
+            }
+
+            var newWeakHandlers = new WeakReference[weakHandlerCount - weakHandlersToDelete.Count];
+            int index = 0;
+            foreach (var weakHandler in weakHandlers)
+            {
+                // Skips the event handler garbage collected
+                if (weakHandlersToDelete.Contains(weakHandler))
+                    continue;
+
+                newWeakHandlers[index++] = weakHandler;
+            }
+
+            // Updatea the weak event handler collection.
+            _weakHandlers = newWeakHandlers;
+        }
+
+        #endregion
+    }
+
+    #endregion
+
+    /// <summary>
+    /// Provides storage for event handlers that garbage collector can collect.
+    /// </summary>
+    public class WeakEvent
+    {
+        WeakEventImplementation<EventHandler> _implementation = new WeakEventImplementation<EventHandler>();
+
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        public WeakEvent() { }
+
+        #endregion
+
+
+        #region Public Methods
+
+        /// <summary>
+        /// Adds an event handler.
+        /// </summary>
+        /// <param name="value">The event handler to add.</param>
+        public void Add(EventHandler value)
+        {
+            _implementation.Add(value);
+        }
+
+        /// <summary>
+        /// Removes the event handler.
+        /// </summary>
+        /// <param name="value">The event handler to remove.</param>
+        public void Remove(EventHandler value)
+        {
+            _implementation.Remove(value);
+        }
+
+        /// <summary>
+        /// Raises an event.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        public void Raise(object sender)
+        {
+            if (sender == null)
+                throw new ArgumentNullException("sender");
+
+            _implementation.Raise(handler => handler(sender, EventArgs.Empty));
+        }
+
+        #endregion
+    }
+
+    /// <summary>
+    /// Provides storage for event handlers that garbage collector can collect.
+    /// </summary>
+    /// <typeparam name="TEventArgs">The event argument type.</typeparam>
+    public class WeakEvent<TEventArgs>
+#if !NICENIS_4C
+        where TEventArgs : class
+#else
+        where TEventArgs : EventArgs
+#endif
+    {
+        WeakEventImplementation<EventHandler<TEventArgs>> _implementation = new WeakEventImplementation<EventHandler<TEventArgs>>();
+
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        public WeakEvent() { }
+
+        #endregion
+
+
+        #region Public Methods
+
+        /// <summary>
+        /// Adds an event handler.
+        /// </summary>
+        /// <param name="value">The event handler to add.</param>
+        public void Add(EventHandler<TEventArgs> value)
+        {
+            _implementation.Add(value);
+        }
+
+        /// <summary>
+        /// Removes the event handler.
+        /// </summary>
+        /// <param name="value">The event handler to remove.</param>
+        public void Remove(EventHandler<TEventArgs> value)
+        {
+            _implementation.Remove(value);
+        }
+
+        /// <summary>
+        /// Raises an event.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The event arguments.</param>
+        public void Raise(object sender, TEventArgs e)
+        {
+            if (sender == null)
+                throw new ArgumentNullException("sender");
+
+            if (e == null)
+                throw new ArgumentNullException("e");
+
+            _implementation.Raise(handler => handler(sender, e));
+        }
+
+        #endregion
+    }
+}
---
+++ Nicenis/NicenisTests/WeakEventTests.cs
@@ -0,0 +1,463 @@
+﻿/*
+ * Author   JO Hyeong-Ryeol
+ * Since    2014.09.18
+ * Version  $Id$
+ * 
+ * This file is a part of the Nicenis project.
+ * https://nicenis.codeplex.com
+ * 
+ * Copyright (C) 2014 JO Hyeong-Ryeol. All rights reserved.
+ */
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nicenis;
+using System;
+using System.Collections;
+
+namespace NicenisTests
+{
+    [TestClass]
+    public class WeakEventTests
+    {
+        #region Helpers
+
+        int _raiseCount = 0;
+        int _raiseCount2 = 0;
+
+        private void EventHandler_Increase_RaiseCount(object sender, EventArgs e)
+        {
+            _raiseCount++;
+        }
+
+        private void EventHandler_Increase_RaiseCount2(object sender, EventArgs e)
+        {
+            _raiseCount2++;
+        }
+
+        static int _staticRaiseCount = 0;
+        static int _staticRaiseCount2 = 0;
+
+        private static void EventHandler_Increase_StaticRaiseCount(object sender, EventArgs e)
+        {
+            _staticRaiseCount++;
+        }
+
+        private static void EventHandler_Increase_StaticRaiseCount2(object sender, EventArgs e)
+        {
+            _staticRaiseCount2++;
+        }
+
+        class WeakEventTest
+        {
+            WeakEvent _weakEvent;
+            int _value = 0;
+
+
+            #region Constructors
+
+            public WeakEventTest(WeakEvent weakEvent)
+            {
+                if (weakEvent == null)
+                    throw new ArgumentNullException("weakEvent");
+
+                _weakEvent = weakEvent;
+            }
+
+            #endregion
+
+
+            #region Helpers
+
+            private void IncreaseStaticRaiseCount(object sender, EventArgs e)
+            {
+                _staticRaiseCount++;
+            }
+
+            private void IncreaseStaticRaiseCount2(object sender, EventArgs e)
+            {
+                _staticRaiseCount2++;
+            }
+
+            private static void StaticIncreaseStaticRaiseCount(object sender, EventArgs e)
+            {
+                _staticRaiseCount++;
+            }
+
+            private static void StaticIncreaseStaticRaiseCount2(object sender, EventArgs e)
+            {
+                _staticRaiseCount2++;
+            }
+
+            #endregion
+
+
+            #region Public Methods
+
+            public void Add_Instance_Method_Increase_StaticRaiseCount()
+            {
+                _weakEvent.Add(IncreaseStaticRaiseCount);
+            }
+
+            public void Add_Instance_Method_Increase_StaticRaiseCount2()
+            {
+                _weakEvent.Add(IncreaseStaticRaiseCount2);
+            }
+
+            public void Add_Static_Method_Increase_StaticRaiseCount()
+            {
+                _weakEvent.Add(StaticIncreaseStaticRaiseCount);
+            }
+
+            public void Add_Static_Method_Increase_StaticRaiseCount2()
+            {
+                _weakEvent.Add(StaticIncreaseStaticRaiseCount2);
+            }
+
+            public void Add_Instance_Lambda_Increase_StaticRaiseCount()
+            {
+                _weakEvent.Add((_, __) => { _value++; _staticRaiseCount++; });
+            }
+
+            public void Add_Instance_Lambda_Increase_StaticRaiseCount2()
+            {
+                _weakEvent.Add((_, __) => { _value++; _staticRaiseCount2++; });
+            }
+
+            public void Add_Static_Lambda_Increase_StaticRaiseCount()
+            {
+                _weakEvent.Add((_, __) => _staticRaiseCount++);
+            }
+
+            public void Add_Static_Lambda_Increase_StaticRaiseCount2()
+            {
+                _weakEvent.Add((_, __) => _staticRaiseCount2++);
+            }
+
+            #endregion
+        }
+
+        #endregion
+
+        [TestMethod]
+        public void WeakEvent_Must_Call_EventHandler_Added_By_One_Inline_Instance_Lambda()
+        {
+            // arrange
+            WeakEvent weakEvent = new WeakEvent();
+            _raiseCount = 0;
+            weakEvent.Add((_, __) => _raiseCount++);
+
+            // act
+            weakEvent.Raise(this);
+
+            // assert
+            Assert.IsTrue(_raiseCount == 1);
+        }
+
+        [TestMethod]
+        public void WeakEvent_Must_Call_EventHandler_Added_By_One_Inline_Static_Lambda()
+        {
+            // arrange
+            WeakEvent weakEvent = new WeakEvent();
+            _staticRaiseCount = 0;
+            weakEvent.Add((_, __) => _staticRaiseCount++);
+
+            // act
+            weakEvent.Raise(this);
+
+            // assert
+            Assert.IsTrue(_staticRaiseCount == 1);
+        }
+
+        [TestMethod]
+        public void WeakEvent_Must_Call_EventHandler_Added_By_One_Instance_Method()
+        {
+            // arrange
+            WeakEvent weakEvent = new WeakEvent();
+            _raiseCount = 0;
+            weakEvent.Add(EventHandler_Increase_RaiseCount);
+
+            // act
+            weakEvent.Raise(this);
+
+            // assert
+            Assert.IsTrue(_raiseCount == 1);
+        }
+
+        [TestMethod]
+        public void WeakEvent_Must_Call_EventHandler_Added_By_One_Static_Method()
+        {
+            // arrange
+            WeakEvent weakEvent = new WeakEvent();
+            _staticRaiseCount = 0;
+            weakEvent.Add(EventHandler_Increase_StaticRaiseCount);
+
+            // act
+            weakEvent.Raise(this);
+
+            // assert
+            Assert.IsTrue(_staticRaiseCount == 1);
+        }
+
+        [TestMethod]
+        public void WeakEvent_Must_Call_EventHandler_Added_By_Two_Inline_Instance_Lambda()
+        {
+            // arrange
+            WeakEvent weakEvent = new WeakEvent();
+            _raiseCount = 0;
+            _raiseCount2 = 0;
+            weakEvent.Add((_, __) => _raiseCount++);
+            weakEvent.Add((_, __) => _raiseCount2++);
+
+            // act
+            weakEvent.Raise(this);
+
+            // assert
+            Assert.IsTrue(_raiseCount == 1);
+            Assert.IsTrue(_raiseCount2 == 1);
+        }
+
+        [TestMethod]
+        public void WeakEvent_Must_Call_EventHandler_Added_By_Two_Inline_Static_Lambda()
+        {
+            // arrange
+            WeakEvent weakEvent = new WeakEvent();
+            _staticRaiseCount = 0;
+            _staticRaiseCount2 = 0;
+            weakEvent.Add((_, __) => _staticRaiseCount++);
+            weakEvent.Add((_, __) => _staticRaiseCount2++);
+
+            // act
+            weakEvent.Raise(this);
+
+            // assert
+            Assert.IsTrue(_staticRaiseCount == 1);
+            Assert.IsTrue(_staticRaiseCount2 == 1);
+        }
+
+        [TestMethod]
+        public void WeakEvent_Must_Call_EventHandler_Added_By_Two_Instance_Method()
+        {
+            // arrange
+            WeakEvent weakEvent = new WeakEvent();
+            _raiseCount = 0;
+            _raiseCount2 = 0;
+            weakEvent.Add(EventHandler_Increase_RaiseCount);
+            weakEvent.Add(EventHandler_Increase_RaiseCount2);
+
+            // act
+            weakEvent.Raise(this);
+
+            // assert
+            Assert.IsTrue(_raiseCount == 1);
+            Assert.IsTrue(_raiseCount2 == 1);
+        }
+
+        [TestMethod]
+        public void WeakEvent_Must_Call_EventHandler_Added_By_Two_Static_Method()
+        {
+            // arrange
+            WeakEvent weakEvent = new WeakEvent();
+            _staticRaiseCount = 0;
+            _staticRaiseCount2 = 0;
+            weakEvent.Add(EventHandler_Increase_StaticRaiseCount);
+            weakEvent.Add(EventHandler_Increase_StaticRaiseCount2);
+
+            // act
+            weakEvent.Raise(this);
+
+            // assert
+            Assert.IsTrue(_staticRaiseCount == 1);
+            Assert.IsTrue(_staticRaiseCount2 == 1);
+        }
+
+        [TestMethod]
+        public void WeakEvent_Does_Not_Support_Removing_EventHandler_By_Inline_Instance_Lambda()
+        {
+            // arrange
+            WeakEvent weakEvent = new WeakEvent();
+            _raiseCount = 0;
+            _raiseCount2 = 0;
+            weakEvent.Add((_, __) => _raiseCount++);
+            weakEvent.Add((_, __) => _raiseCount2++);
+            weakEvent.Remove((_, __) => _raiseCount2++);
+
+            // act
+            weakEvent.Raise(this);
+
+            // assert
+            Assert.IsTrue(_raiseCount == 1);
+            Assert.IsTrue(_raiseCount2 == 1);
+        }
+
+        [TestMethod]
+        public void WeakEvent_Does_Not_Support_Removing_EventHandler_By_Inline_Static_Lambda()
+        {
+            // arrange
+            WeakEvent weakEvent = new WeakEvent();
+            _staticRaiseCount = 0;
+            _staticRaiseCount2 = 0;
+            weakEvent.Add((_, __) => _staticRaiseCount++);
+            weakEvent.Add((_, __) => _staticRaiseCount2++);
+            weakEvent.Remove((_, __) => _staticRaiseCount2++);
+
+            // act
+            weakEvent.Raise(this);
+
+            // assert
+            Assert.IsTrue(_staticRaiseCount == 1);
+            Assert.IsTrue(_staticRaiseCount2 == 1);
+        }
+
+        [TestMethod]
+        public void WeakEvent_Must_Not_Call_EventHandler_Removed_By_Instance_Lambda()
+        {
+            // arrange
+            WeakEvent weakEvent = new WeakEvent();
+            _raiseCount = 0;
+            _raiseCount2 = 0;
+            EventHandler eventHandler = (_, __) => _raiseCount++;
+            EventHandler eventHandler2 = (_, __) => _raiseCount2++;
+            weakEvent.Add(eventHandler);
+            weakEvent.Add(eventHandler2);
+            weakEvent.Remove(eventHandler2);
+
+            // act
+            weakEvent.Raise(this);
+
+            // assert
+            Assert.IsTrue(_raiseCount == 1);
+            Assert.IsTrue(_raiseCount2 == 0);
+        }
+
+        [TestMethod]
+        public void WeakEvent_Must_Not_Call_EventHandler_Removed_By_Static_Lambda()
+        {
+            // arrange
+            WeakEvent weakEvent = new WeakEvent();
+            _staticRaiseCount = 0;
+            _staticRaiseCount2 = 0;
+            EventHandler eventHandler = (_, __) => _staticRaiseCount++;
+            EventHandler eventHandler2 = (_, __) => _staticRaiseCount2++;
+            weakEvent.Add(eventHandler);
+            weakEvent.Add(eventHandler2);
+            weakEvent.Remove(eventHandler2);
+
+            // act
+            weakEvent.Raise(this);
+
+            // assert
+            Assert.IsTrue(_staticRaiseCount == 1);
+            Assert.IsTrue(_staticRaiseCount2 == 0);
+        }
+
+        [TestMethod]
+        public void WeakEvent_Must_Not_Call_EventHandler_Removed_By_Instance_Method()
+        {
+            // arrange
+            WeakEvent weakEvent = new WeakEvent();
+            _raiseCount = 0;
+            _raiseCount2 = 0;
+            weakEvent.Add(EventHandler_Increase_RaiseCount);
+            weakEvent.Add(EventHandler_Increase_RaiseCount2);
+            weakEvent.Remove(EventHandler_Increase_RaiseCount2);
+
+            // act
+            weakEvent.Raise(this);
+
+            // assert
+            Assert.IsTrue(_raiseCount == 1);
+            Assert.IsTrue(_raiseCount2 == 0);
+        }
+
+        [TestMethod]
+        public void WeakEvent_Must_Not_Call_EventHandler_Removed_By_Static_Method()
+        {
+            // arrange
+            WeakEvent weakEvent = new WeakEvent();
+            _staticRaiseCount = 0;
+            _staticRaiseCount2 = 0;
+            weakEvent.Add(EventHandler_Increase_StaticRaiseCount);
+            weakEvent.Add(EventHandler_Increase_StaticRaiseCount2);
+            weakEvent.Remove(EventHandler_Increase_StaticRaiseCount2);
+
+            // act
+            weakEvent.Raise(this);
+
+            // assert
+            Assert.IsTrue(_staticRaiseCount == 1);
+            Assert.IsTrue(_staticRaiseCount2 == 0);
+        }
+
+        [TestMethod]
+        public void WeakEvent_Must_Not_Call_Instance_Method_EventHandler_Removed_By_Garbage_Collector()
+        {
+            // arrange
+            WeakEvent weakEvent = new WeakEvent();
+            WeakEventTest weakEventTest = new WeakEventTest(weakEvent);
+            _staticRaiseCount = 0;
+            weakEventTest.Add_Instance_Method_Increase_StaticRaiseCount();
+            weakEventTest = null;
+            GC.Collect();
+
+            // act
+            weakEvent.Raise(this);
+
+            // assert
+            Assert.IsTrue(_staticRaiseCount == 0);
+        }
+
+        [TestMethod]
+        public void WeakEvent_Must_Not_Call_Static_Method_EventHandler_Removed_By_Garbage_Collector()
+        {
+            // arrange
+            WeakEvent weakEvent = new WeakEvent();
+            WeakEventTest weakEventTest = new WeakEventTest(weakEvent);
+            _staticRaiseCount = 0;
+            weakEventTest.Add_Static_Method_Increase_StaticRaiseCount();
+            weakEventTest = null;
+            GC.Collect();
+
+            // act
+            weakEvent.Raise(this);
+
+            // assert
+            Assert.IsTrue(_staticRaiseCount == 0);
+        }
+
+        [TestMethod]
+        public void WeakEvent_Must_Not_Call_Instance_Lambda_EventHandler_Removed_By_Garbage_Collector()
+        {
+            // arrange
+            WeakEvent weakEvent = new WeakEvent();
+            WeakEventTest weakEventTest = new WeakEventTest(weakEvent);
+            _staticRaiseCount = 0;
+            weakEventTest.Add_Instance_Lambda_Increase_StaticRaiseCount();
+            weakEventTest = null;
+            GC.Collect();
+
+            // act
+            weakEvent.Raise(this);
+
+            // assert
+            Assert.IsTrue(_staticRaiseCount == 0);
+        }
+
+        [TestMethod]
+        public void WeakEvent_Does_Not_Support_Removing_Static_Lambda_EventHandler_Removed_By_Garbage_Collector()
+        {
+            // arrange
+            WeakEvent weakEvent = new WeakEvent();
+            WeakEventTest weakEventTest = new WeakEventTest(weakEvent);
+            _staticRaiseCount = 0;
+            weakEventTest.Add_Static_Lambda_Increase_StaticRaiseCount();
+            weakEventTest = null;
+            GC.Collect();
+
+            // act
+            weakEvent.Raise(this);
+
+            // assert
+            Assert.IsTrue(_staticRaiseCount == 1);
+        }
+    }
+}
