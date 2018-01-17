5f66a94231b3777c54b933f67306e15847b2a79c
JO Hyeong-Ryeol <hyeongryeol@hotmail.com>
2018-01-18 00:57:30 +0900

    VoidToggleButton is added.

---
+++ Nicenis/Nicenis/Windows/Controls/VoidToggleButton.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Author   JO Hyeong-Ryeol
+ * Since    2018.01.18
+ * 
+ * This file is a part of the Nicenis project.
+ * https://github.com/nicenis/nicenis
+ * 
+ * Copyright (C) 2018 JO Hyeong-Ryeol. All rights reserved.
+ */
+
+using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace Nicenis.Windows.Controls
+{
+    /// <summary>
+    /// Represents a toggle button without any visual element.
+    /// </summary>
+    public class VoidToggleButton : ToggleButton
+    {
+        #region Constructors
+
+        /// <summary>
+        /// The static constructor.
+        /// </summary>
+        static VoidToggleButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(VoidToggleButton), new FrameworkPropertyMetadata(typeof(VoidToggleButton)));
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the VoidToggleButton class.
+        /// </summary>
+        public VoidToggleButton() { }
+
+        #endregion
+    }
+}
